39f29a8655a267d3e32c2822438e3624295a1496
Brent E <brentett@gmail.com>
2022-10-09 15:21:18 -0400

    Update RentalAgreement.java
    WIP - Added draft rental agreement pojo based on requirements.

--- pos-demo/src/main/java/com/bteposdemo/posdemo/RentalAgreement.java
+++ pos-demo/src/main/java/com/bteposdemo/posdemo/RentalAgreement.java
@@ -7,8 +7,18 @@ import java.util.Date;
 public class RentalAgreement {
     private RentalTool rentalTool;
     private int rentalDayCount;
-    private double discountPercent;
+
     private Date checkoutDate;
+    private Date dueDate;
+
+    private double dailyRentalChargeRate;
+    private int chargeableRentalDays;
+
+    private double prediscountTotalCost;
+    private double discountPercent;
+    private double discountedCostAmount;
+    private double finalTotalCost;
+
 
 
     void printRentalAgreement() {
