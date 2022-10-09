196fdf0203563bed431442f8e42788ff7de4de01
Brent E <brentett@gmail.com>
2022-10-09 14:59:14 -0400

    Update RentalTool.java
    Added constants to rental tool enum to store tool type and brand.

--- pos-demo/src/main/java/com/bteposdemo/staticdata/RentalTool.java
+++ pos-demo/src/main/java/com/bteposdemo/staticdata/RentalTool.java
@@ -1,6 +1,16 @@
 package com.bteposdemo.staticdata;
 
 public enum RentalTool {
+    CHNS("Chainsaw","Stihl"),
+    LADW("Ladder","Werner"),
+    JAKD("Jackhammer","DeWalt"),
+    JAKR("Jackhammer","Ridgid");
 
+    private String type;
+    private String brand;
 
+    RentalTool(String type, String brand) {
+        this.type = type;
+        this.brand = brand;
+    }
 }
