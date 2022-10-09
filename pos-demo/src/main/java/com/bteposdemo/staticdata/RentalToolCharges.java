38222a916c9a508b523b9dd3359ab829b11754b3
Brent E <brentett@gmail.com>
2022-10-09 15:08:58 -0400

    Update RentalToolCharges.java
    Aded rental tool charge constants to enum for keeping track of charge price and when to charge.

--- pos-demo/src/main/java/com/bteposdemo/staticdata/RentalToolCharges.java
+++ pos-demo/src/main/java/com/bteposdemo/staticdata/RentalToolCharges.java
@@ -1,4 +1,19 @@
 package com.bteposdemo.staticdata;
 
 public enum RentalToolCharges {
+    Ladder(1.99, true, true, false),
+    Chainsaw(1.49, true, false, true),
+    Jackhammer(2.99, true, false, false);
+
+    private double dailyChargePrice;
+    private boolean weekdayCharge;
+    private boolean weekendCharge;
+    private boolean holidayCharge;
+
+    RentalToolCharges(double dailyChargePrice, boolean weekdayCharge, boolean weekendCharge, boolean holidayCharge) {
+        this.dailyChargePrice = dailyChargePrice;
+        this.weekdayCharge = weekdayCharge;
+        this.weekendCharge = weekendCharge;
+        this.holidayCharge = holidayCharge;
+    }
 }
