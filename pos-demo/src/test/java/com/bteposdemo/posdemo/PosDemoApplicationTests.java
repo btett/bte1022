4743b7823ebba3ac4a71d23e7c1d7ae81580a53a
Brent E <brentett@gmail.com>
2022-10-09 13:10:09 -0400

    Initial project template
    Initial commit, project template created using Spring Initializer tool

---
+++ pos-demo/src/test/java/com/bteposdemo/posdemo/PosDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bteposdemo.posdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PosDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
